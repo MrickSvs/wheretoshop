5cf84c5c9aad4267cb55454bca2318c9a637d219
Rikem <aymeric.saves@gmail.com>
2023-09-06 00:20:13 +0200

    filtre

--- server/routes/shops.js
+++ server/routes/shops.js
@@ -1,15 +1,20 @@
 const express = require('express');
 const router = express.Router();
-const Shop = require('../models/shops'); // Assurez-vous que le chemin est correct
+const Shop = require('../models/shops');
 
-// Endpoint pour récupérer tous les shops
 router.get('/', async (req, res) => {
-    try {
-        const shops = await Shop.find();
-        res.json(shops);
-    } catch (err) {
-        res.status(500).json({ message: err.message });
+  try {
+    const style = req.query.style;
+    let shops;
+    if (style) {
+      shops = await Shop.find({ style });
+    } else {
+      shops = await Shop.find();
     }
+    res.json(shops);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
 });
 
 module.exports = router;
